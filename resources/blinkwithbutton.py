d5e1e5941b25eeaa35bed9634695813647b0041a
Zinbal <97798100+zinbal@users.noreply.github.com>
2022-01-16 05:37:05 +0000

    Update blinkwithbutton.py

--- resources/blinkwithbutton.py
+++ resources/blinkwithbutton.py
@@ -1,5 +1,5 @@
 '''
-    this code keeps led on untill the button is pressed again modifying it is almost same as modifying arduino code but keep in mind of gpio pins
+    this code keeps led on till the button is pressed  modifying it is almost same as modifying arduino code but keep in mind of gpio pins
     connect your push button with a +3.3v , a resistor of 3.3 K ohm going to grnd and oine point to GPIO pin 17
     connect led with 220ohm resistance to common grnd and other otput to GPIO pin 4
 '''
@@ -9,16 +9,14 @@ GPIO.setmode(GPIO.BCM)
 led=4;btn=17
 GPIO.setup(led,GPIO.OUT)
 GPIO.setup(btn,GPIO.IN)
-count=0
-while True:    
-    if GPIO.input(btn)==GPIO.HIGH:
-       count+=1
-       time.sleep(.2)
-    print(count)
-    if count%2==0:
-        GPIO.output(led,GPIO.HIGH)
-    else:
-        GPIO.output(led,GPIO.LOW)
-    time.sleep(.05)
-    
-GPIO.cleanup()
+try:
+    while True:    
+        if GPIO.input(btn)==GPIO.HIGH:
+            GPIO.output(led,GPIO.HIGH)
+        else:
+            GPIO.output(led,GPIO.LOW)
+        time.sleep(.05)
+        
+    GPIO.cleanup()
+except:
+    GPIO.cleanup()
