61089a2e0bbe3eb6e397c8f859d8f8c57d2a85c8
Zinbal <97798100+zinbal@users.noreply.github.com>
2022-01-16 05:35:57 +0000

    Update led blink.py

--- resources/led blink.py
+++ resources/led blink.py
@@ -1,14 +1,18 @@
 '''
     this code blinks led by keeping it on for 1.5 sec and offfor 1 sec you can change these value from below time.sleep function
+    Just connect led with GPIO pin 4 and a resistor of 220 ohm or better go with color code for resistors
 '''
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 led=4
 GPIO.setup(led,GPIO.OUT)
-for i in range(0,10):    
-    GPIO.output(led,GPIO.HIGH)
-    time.sleep(1.5)
-    GPIO.output(led,GPIO.LOW)
-    time.sleep(1)
-GPIO.cleanup()
+try:
+    for i in range(0,5):    
+        GPIO.output(led,GPIO.HIGH)
+        time.sleep(1.5)
+        GPIO.output(led,GPIO.LOW)
+        time.sleep(1)
+    GPIO.cleanup()
+except:
+    GPIO.cleanup()
