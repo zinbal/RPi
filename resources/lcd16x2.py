90e5abe9fbc8793df83e7f4b450d385a6b4666ca
Zinbal <97798100+zinbal@users.noreply.github.com>
2022-01-24 19:18:03 +0000

    Update and rename lcd_16x2_backlight.py to lcd16x2.py

--- resources/lcd16x2.py
+++ resources/lcd16x2.py
@@ -1,56 +1,3 @@
-#!/usr/bin/python
-#--------------------------------------
-#    ___  ___  _ ____
-#   / _ \/ _ \(_) __/__  __ __
-#  / , _/ ___/ /\ \/ _ \/ // /
-# /_/|_/_/  /_/___/ .__/\_, /
-#                /_/   /___/
-#
-#  lcd_16x2.py
-#  16x2 LCD Test Script with
-#  backlight control and text justification
-#
-# Author : Matt Hawkins
-# Date   : 06/04/2015
-#
-# http://www.raspberrypi-spy.co.uk/
-#
-# Copyright 2015 Matt Hawkins
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#--------------------------------------
-
-# The wiring for the LCD is as follows:
-# 1 : GND
-# 2 : 5V
-# 3 : Contrast (0-5V)*
-# 4 : RS (Register Select)
-# 5 : R/W (Read Write)       - GROUND THIS PIN
-# 6 : Enable or Strobe
-# 7 : Data Bit 0             - NOT USED
-# 8 : Data Bit 1             - NOT USED
-# 9 : Data Bit 2             - NOT USED
-# 10: Data Bit 3             - NOT USED
-# 11: Data Bit 4
-# 12: Data Bit 5
-# 13: Data Bit 6
-# 14: Data Bit 7
-# 15: LCD Backlight +5V**
-# 16: LCD Backlight GND
-
-#import
 import RPi.GPIO as GPIO
 import time
 
@@ -99,31 +46,6 @@ def main():
   lcd_backlight(True)
   time.sleep(0.5)
 
-  while True:
-
-    # Send some centred text
-    lcd_string("Rasbperry Pi",LCD_LINE_1,2)
-    lcd_string("16x2 LCD Test",LCD_LINE_2,2)
-
-    time.sleep(3) # 3 second delay
-
-    # Send some left justified text
-    lcd_string("1234567890123456",LCD_LINE_1,1)
-    lcd_string("abcdefghijklmnop",LCD_LINE_2,1)
-
-    time.sleep(3) # 3 second delay
-
-    # Send some right justified text
-    lcd_string("Raspberrypi-spy",LCD_LINE_1,3)
-    lcd_string(".co.uk",LCD_LINE_2,3)
-
-    time.sleep(3) # 20 second delay
-
-    # Send some centred text
-    lcd_string("Follow me on",LCD_LINE_1,2)
-    lcd_string("Twitter @RPiSpy",LCD_LINE_2,2)
-
-    time.sleep(3)
 
 def lcd_init():
   # Initialise display
@@ -208,9 +130,10 @@ def lcd_backlight(flag):
   GPIO.output(LED_ON, flag)
 
 if __name__ == '__main__':
-
   try:
     main()
+    lcd_string("hello world",LCD_LINE_1,2)
+    time.sleep(2)
   except KeyboardInterrupt:
     pass
   finally:
